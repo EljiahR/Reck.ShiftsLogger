645deeed99f0f940920a1107be9c46a398ae8da7
Elijah Reck <kcerhajile@gmail.com>
2024-06-04 19:25:00 -0400

    Created shiftlog model

---
+++ ShiftsLoggerAPI/Models/ShiftLogContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShiftsLoggerAPI.Models;
+
+public class ShiftLogContext : DbContext
+{
+    public ShiftLogContext(DbContextOptions<ShiftLogContext> options) 
+        : base(options) { }
+
+}
