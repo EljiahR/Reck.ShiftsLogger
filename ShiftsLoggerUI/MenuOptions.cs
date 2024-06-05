cebb26f128cc0be91c1bab8c8af4a07392954e5b
Elijah Reck <kcerhajile@gmail.com>
2024-06-05 16:21:16 -0400

    Created UI and Main Menu

---
+++ ShiftsLoggerUI/MenuOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftsLoggerUI
+{
+    internal class MenuOptions
+    {
+        public static readonly string AddNewShift = "Add New Shift";
+        public static readonly string ViewEditShift = "View/Edit Shifts";
+        public static readonly string DeleteShift = "Delete Existing Shift";
+        public static readonly string Exit = "Exit";
+
+
+        public static readonly string[] MainMenu = [AddNewShift, ViewEditShift, DeleteShift, Exit];
+    }
+}
---
+++ ShiftsLoggerUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using ShiftsLoggerUI;
+using Spectre.Console;
+
+string? menuOption;
+do
+{
+    menuOption = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+        .Title("Main Menu")
+        .PageSize(MenuOptions.MainMenu.Count())
+        .AddChoices(MenuOptions.MainMenu));
+
+    switch(menuOption)
+    {
+
+    }
+} while (menuOption != MenuOptions.Exit);
